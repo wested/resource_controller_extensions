cece556eb389cae9e065d1c20933c77134529b61
Andrew Carpenter <acarpen@wested.org>
2009-05-27 10:16:02 -0700

    updated base tests to be more in line with rest

--- test/resource_controller_extensions_test.rb
+++ test/resource_controller_extensions_test.rb
@@ -1,46 +1,41 @@
 require File.dirname(__FILE__) + '/test_helper'
 
-class ResourceControllerExtensionsTest < Test::Unit::TestCase
+class ResourceControllerExtensionsTest < ActionController::TestCase
+  class HomeController < ActionController::Base
+  end
+
+  class ProjectsController < ActionController::Base
+    resource_controller
+  end
+  
+  tests ProjectsController
   context "A controller that has called resource_controller" do
-    setup do
-      class ApplicationController < ActionController::Base
-      end
-
-      class HomeController < ApplicationController
-      end
-
-      class UsersController < ApplicationController
-        resource_controller
-      end
-      @controller_class = UsersController
-    end
-    
     should "have class method add_extensions" do
-      assert @controller_class.respond_to?(:add_extensions)
+      assert ProjectsController.respond_to?(:add_extensions)
     end
     
     
     context "when add_extension is called with :delete_action" do
       should "include the DeleteAction" do
-        @controller_class.instance_eval do
+        ProjectsController.instance_eval do
           add_extensions :delete
         end
-        assert_includes @controller_class.included_modules, ResourceControllerExtensions::DeleteAction
+        assert_includes ProjectsController.included_modules, ResourceControllerExtensions::DeleteAction
       end
     end
     
     context "when add_extension is called with :searchlogic_collection" do
       should "include SearchlogicCollection" do
-        @controller_class.instance_eval do
+        ProjectsController.instance_eval do
           add_extensions :searchlogic
         end
-        assert_includes @controller_class.included_modules, ResourceControllerExtensions::SearchlogicCollection
+        assert_includes ProjectsController.included_modules, ResourceControllerExtensions::SearchlogicCollection
       end
     end
     
     context "when add_extension is called with :all" do
       should "include DeleteAction and SearchlogicCollection" do
-        @controller_class.instance_eval do
+        ProjectsController.instance_eval do
           add_extensions :all
         end
         
@@ -52,14 +47,14 @@ class ResourceControllerExtensionsTest < Test::Unit::TestCase
           ResourceControllerExtensions::TransactionalUpdateAction,
           ResourceControllerExtensions::UpdateAllAction
         ].each do |mod|
-          assert_includes @controller_class.included_modules, mod
+          assert_includes ProjectsController.included_modules, mod
         end
       end
     end
     
     context "when add_extension is called with :foobar" do
       should_raise(ArgumentError) do
-        @controller_class.instance_eval do
+        ProjectsController.instance_eval do
           add_extensions :foobar
         end
       end
@@ -69,23 +64,19 @@ class ResourceControllerExtensionsTest < Test::Unit::TestCase
   
   context "a controller calling add_extension twice" do
     should "include both extensions" do
-      UsersController.instance_eval do
+      ProjectsController.instance_eval do
         add_extensions :delete
         add_extensions :searchlogic
       end
       
-      assert_includes UsersController.included_modules, ResourceControllerExtensions::DeleteAction
-      assert_includes UsersController.included_modules, ResourceControllerExtensions::SearchlogicCollection
+      assert_includes ProjectsController.included_modules, ResourceControllerExtensions::DeleteAction
+      assert_includes ProjectsController.included_modules, ResourceControllerExtensions::SearchlogicCollection
     end
   end
   
   context "A controller that has not called resource_controller" do
-    setup do
-      @controller_class = HomeController
-    end
-    
     should "not have class method add_extensions" do
-      assert ! @controller_class.respond_to?(:add_extensions)
+      assert ! HomeController.respond_to?(:add_extensions)
     end
   end
 end
